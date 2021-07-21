a10835d07ff44320d801f7b6b935cae2a14aed03
sergiocardenasg <sergiocardenas@utexas.edu>
2021-07-20 21:18:39 -0500

    work on sectors fetch request

---
+++ stocks_client/index.js
@@ -0,0 +1,5 @@
+const baseURL = "http://127.0.0.1:3000"
+
+const sectorService = new SectorService(baseURL)
+
+sectorService.getSectors()
---
+++ stocks_client/src/sectorService.js
@@ -0,0 +1,13 @@
+class SectorService {
+    constructor(endpoint) {
+        this.endpoint = endpoint
+    }
+
+    getSectors() {
+        fetch(`${this.endpoint}/sectors`)
+        .then(resp => resp.json())
+        .then(sectors => {
+            debugger
+        })
+    }
+}
